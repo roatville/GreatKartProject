c64df735b6fa9531e11f06a695c17c81245c9198
Roatville <Roatvilless@gmail.com>
2023-06-30 02:18:58 +0530

    update

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'greatkart-study-ecommerce.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://hmstest007.pythonanywhere.com/']
 
 
 # Application definition
